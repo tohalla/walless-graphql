8717397a19b18b7bdb0d76b6e94ef39930edbdc9
Touko Hallasmaa <touko.hallasmaa@outlook.com>
2017-04-21 18:15:59 +0300

    add queries and mutations for serving locations

---
+++ restaurant/servingLocation.mutations.js
@@ -0,0 +1,54 @@
+// @flow
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+
+import {servingLocationFragment} from './servingLocation.queries';
+
+const createServingLocation = graphql(
+  gql`
+  mutation createServingLocation($servingLocation: CreateServingLocationInput!) {
+    createServingLocation(input: $servingLocation) {
+      servingLocation {
+        ...servingLocationInfo
+      }
+    }
+  }
+  ${servingLocationFragment}
+  `, {
+    props: ({mutate, data}) => ({
+      createServingLocation: servingLocation => mutate({
+        variables: {servingLocation: {servingLocation}}
+      })
+    })
+  }
+);
+
+const updateServingLocation = graphql(
+  gql`
+  mutation updateServingLocationById($input: UpdateServingLocationByIdInput!) {
+    updateServingLocationById(input: $input) {
+      servingLocation {
+        ...servingLocationInfo
+      }
+    }
+  }
+  ${servingLocationFragment}
+  `, {
+    props: ({mutate}) => ({
+      updateServingLocation: (servingLocation: {id: Number}) => mutate({
+        variables: {
+          input: {
+            id: servingLocation.id,
+            servingLocationPatch: servingLocation
+          }
+        }
+      })
+    })
+  }
+);
+
+
+export {
+  createServingLocation,
+  updateServingLocation
+};
---
+++ restaurant/servingLocation.queries.js
@@ -0,0 +1,41 @@
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+
+const servingLocationFragment = gql(`
+  fragment servingLocationInfo on ServingLocation {
+    id
+    name
+    enabled
+    createdAt
+  }
+`);
+
+const getServingLocation = graphql(
+  gql`
+    query servingLocationById($id: Int!) {
+      servingLocationById(id: $id) {
+        ...servingLocationInfo
+      }
+    }
+    ${servingLocationFragment}
+  `, {
+    skip: ownProps => typeof ownProps.servingLocation !== 'number',
+    options: ownProps => ({
+      variables: {
+        id: ownProps.servingLocation
+      }
+    }),
+    props: ({ownProps, data}) => {
+      const {servingLocationById, ...rest} = data;
+      return {getServingLocation: {
+        servingLocation: servingLocationById,
+        data: rest
+      }};
+    }
+  }
+);
+
+export {
+  servingLocationFragment,
+  getServingLocation
+};
